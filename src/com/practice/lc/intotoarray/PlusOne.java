0e12bfb1bdffa51b36ed92eac744b0cda2a31787
MarkFernandes11 <markfernandes66@gmail.com>
2023-06-24 18:08:03 +0530

    Added solutions to introduction-to-array section

---
+++ src/com/practice/lc/intotoarray/PlusOne.java
@@ -0,0 +1,43 @@
+package com.practice.lc.intotoarray;
+
+import java.util.Arrays;
+
+public class PlusOne {
+	
+	public static void main(String[] args) {
+		int[] digits = { 9,9 };
+		int[] result = plusOne(digits);
+		
+		System.out.println(Arrays.toString(result));
+	}
+	
+    public static int[] plusOne(int[] digits) {
+        boolean allNines = true;
+        
+        for (int num : digits) {
+			if(num != 9) {
+				allNines = false;
+				break;
+			}
+		}
+        
+        if(allNines) {
+        	int[] result = new int[digits.length + 1];
+        	result[0] = 1;
+        	
+        	return result;
+        }
+        
+        for (int i = digits.length - 1; i >= 0; i--) {
+			if(digits[i] < 9) {
+				digits[i] = digits[i] + 1;
+				break;
+			} else {
+				digits[i] = 0;
+			}
+		}
+    	
+    	return digits;
+    }
+
+}
